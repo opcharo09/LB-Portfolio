b328314af553145b72222d4366179872bcad8197
unknown <oscar.charo09@gmail.com>
2019-03-31 18:47:57 -0500

    media q

--- assets-lb/javascript/game.js
+++ assets-lb/javascript/game.js
@@ -1,11 +1,11 @@
-alert("Welcome: " + "Little Flower");
-alert("Hope you like it");
-alert("Let me know what you think");
-var mad= prompt("why are you mad at me :/?");
-var yes= confirm("will you forgive me, yes(ok) or no(cancel)?");
-if (yes) {
-    document.write("Thank you!!!!!!")}
- else {
-        document.write("hmmm :( :( :( ");
-      }
-alert("I MISS YOU!");
+//alert("Welcome: " + "Little Flower");
+//alert("Hope you like it");
+//alert("Let me know what you think");
+//var mad= prompt("why are you mad at me :/?");
+//var yes= confirm("will you forgive me, yes(ok) or no(cancel)?");
+//if (yes) {
+  //  document.write("Thank you!!!!!!")}
+ //else {
+   //     document.write("hmmm :( :( :( ");
+      
+//alert("I MISS YOU!");
