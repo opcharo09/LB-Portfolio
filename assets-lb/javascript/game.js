e30962e31ef604c84985477df0895e1bdfb1ac86
unknown <oscar.charo09@gmail.com>
2019-04-05 18:57:19 -0500

    java

--- assets-lb/javascript/game.js
+++ assets-lb/javascript/game.js
@@ -1,11 +1,12 @@
-//alert("Welcome: " + "Little Flower");
-//alert("Hope you like it");
-//alert("Let me know what you think");
-//var mad= prompt("why are you mad at me :/?");
-//var yes= confirm("will you forgive me, yes(ok) or no(cancel)?");
-//if (yes) {
-  //  document.write("Thank you!!!!!!")}
- //else {
-   //     document.write("hmmm :( :( :( ");
+alert("Welcome: " + "Little Flower");
+alert("Hope you like it");
+alert("Let me know what you think");
+var mad= prompt("why are you mad at me :/?");
+var yes= confirm("will you forgive me, yes(ok) or no(cancel)?");
+if (yes) {
+    document.write("Thank you!!!!!!")}
+ else {
+        document.write("hmmm :( :( :( ");}
       
-//alert("I MISS YOU!");
+alert("I MISS YOU!");
+alert ("im glad you like the socks and the backpack :)");
